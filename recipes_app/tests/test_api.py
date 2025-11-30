e9a6f8eff6c46c1fb6084678f6647292b66a953d
adminhype <ado.designs@outlook.de>
2025-11-30 23:59:26 +0100

    feat: seconde test POST /recipes-list/

--- recipes_app/tests/test_api.py
+++ recipes_app/tests/test_api.py
@@ -1,5 +1,8 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.contrib.auth.models import User
+
+from recipes_app.models import Recipe
 
 
 class RecipeAPITestCase(APITestCase):
@@ -8,3 +11,20 @@ class RecipeAPITestCase(APITestCase):
         url = '/recipes-list/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_recipe(self):
+
+        user = User.objects.create_user(
+            username='testuser', password='password123')
+
+        url = '/recipes-list/'
+
+        data = {
+            'title': 'pancakes',
+            'description': 'milk, eggs, sugar, flour',
+            'author': user.id
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
