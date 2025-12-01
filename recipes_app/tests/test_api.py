1b9233e07e085f94d736ecd55e8a8f8e61ce493c
adminhype <ado.designs@outlook.de>
2025-12-01 03:40:18 +0100

    feat: happy_path created new recipe as User with token auth

--- recipes_app/tests/test_api.py
+++ recipes_app/tests/test_api.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from django.contrib.auth.models import User
 from recipes_app.models import Recipe
@@ -20,6 +21,7 @@ class RecipeAPITestCaseUnhappy(APITestCase):
 
     def test_create_recipe_without_auth(self):
         response = self.client.post(self.url, self.recipe_data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -27,7 +29,25 @@ class RecipeAPITestCaseHappy(APITestCase):
 
     def setUp(self):
         self.url = '/recipes-list/'
+        self.user = User.objects.create_user(
+            username='chef', password='password123')
+        self.token = Token.objects.create(user=self.user)
+
+        self.recipe_data = {
+            'title': 'steak',
+            'description': 'only for meat lovers',
+            'author': self.user.id
+        }
 
     def test_get_recipes_list(self):
         response = self.client.get(self.url, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_recipe_with_auth(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(self.url, self.recipe_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Recipe.objects.count(), 1)
+        self.assertEqual(Recipe.objects.get().author, self.user)
